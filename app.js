146cbc10af5d4e686539838cfb2994b10267a5e3
Tushar007079 <tusharpanchal21@gnu.ac.in>
2024-02-06 08:54:52 +0530

    This is V3.0 (Tushar Panchal-21162101014)

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-    res.send('JAmes bond here');
-  });
-  const port = process.env.PORT || 3000;
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(port, () => {
   console.log(`API server listening on port ${port}`);
 });
